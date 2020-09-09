81d119fabcf8a331afa86722d77f23d008f3f1a3
Kevin Buman <kevin.buman@students.fhnw.ch>
2020-09-09 10:05:41 +0200

    Added question_randomizer.pl

---
+++ question_randomizer.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use v5.32;
+
+use warnings;
+use diagnostics;
+use Getopt::Long;
+use Try::Catch;
+use File::Slurp;
+use List::Util 'shuffle';
+use experimental 'signatures';
+
+
+state $ascii_header = 
+q(
+
+    ____                  __                _                
+   / __ \____ _____  ____/ /___  ____ ___  (_)___  ___  _____
+  / /_/ / __ `/ __ \/ __  / __ \/ __ `__ \/ /_  / / _ \/ ___/
+ / _, _/ /_/ / / / / /_/ / /_/ / / / / / / / / /_/  __/ /    
+/_/ |_|\__,_/_/ /_/\__,_/\____/_/ /_/ /_/_/ /___/\___/_/     
+                                                             
+v0.1                                               09.09.2020
+                                       
+Created by Kevin Buman                                   
+                                                             
+);
+
+
+
+print_header();
+
+
+sub print_header() {  
+    printf "%s", $ascii_header;
+}
