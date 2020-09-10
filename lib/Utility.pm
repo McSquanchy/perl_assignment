e3599ddc59163e392fe0c5710fce136f353e0124
Kevin Buman <kevin.buman@students.fhnw.ch>
2020-09-10 14:30:27 +0200

    Added Utility.pm

--- lib/Utility.pm
+++ lib/Utility.pm
@@ -1,4 +1,33 @@
-package Utility::Args;
+############################################################
+#
+#                   Print
+#
+############################################################
+
+package Print;
+
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT = qw(print_header);
+
+use experimental 'signatures';
+
+#
+# Print the ascii text at the beginning of the execution
+sub print_header($ascii) {
+    printf "%s", $ascii;
+}
+
+
+
+
+############################################################
+#
+#                   ARGS
+#
+############################################################
+
+package Args;
 
 require Exporter;
 @ISA = qw(Exporter);
