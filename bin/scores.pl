8905be8da68749df493761a25ba128e832d29a3a
Kevin Buman <kevin.buman@students.fhnw.ch>
2020-09-10 18:02:38 +0200

    Modified scores.pl
    Added argument parse subroutine

--- bin/scores.pl
+++ bin/scores.pl
@@ -28,10 +28,57 @@ state $ascii_header = q(
  \______/  \_______/ \______/ |__/       \_______/|_______/ 
                                                             
                                                             
-v0.2                                               09.09.2020
+v0.1                                               09.09.2020
                                        
 Created by Kevin Buman                                   
                                                              
 );
 
+state %args;
+
 Print::print_header($ascii_header);
+
+# Read flags and store values in the hash
+GetOptions( \%args, "master=s", "submissions=s{,}", "help!" )
+  or die( Args::error_args() );
+
+parse_args();
+
+sub parse_args() {
+    my $argc  = keys %args;
+    my @paths = ();
+    if ( $argc > 2 ) {
+        warn "\nToo many arguments. See --help for more information.\n\n";
+        die("too_many_args");
+    }
+    elsif ( $argc == 0 ) {
+        warn "\nToo few arguments. See --help for more information.\n\n";
+        die("too_few_args");
+    }
+    elsif ( $argc == 2 && $args->{"help"} ) {
+        warn "\nWrong usage. See --help for more information.\n\n";
+        die("wrong_args");
+    }
+    elsif ( $argc == 1 && $args->{"help"} ) {
+        _usage();
+        exit(0);
+    }
+    elsif ( $argc != 2 && !( $args->{"master"} && $args->{"submissions"} ) ) {
+        error_args();
+        die("error_args");
+    }
+    else {
+        @paths[0] = $args->{"master"};
+        @paths[1] = $args->{"submissions"};
+    }
+    return @paths;
+}
+
+sub usage {
+    print
+"Usage:\n\trandomizer command syntax:\n\n\t\t./randomizer [options] [arguments]\n\n\tGeneric command options:\n\n";
+    print "\t\t-f, --file:\tSpecify the file to be processed.\n";
+    print "\t\t-o, --output:\tSpecify the output file.\n";
+    print "\t\t-h, --help:\tRead more detailed instructions.\n";
+    print "\n";
+}
